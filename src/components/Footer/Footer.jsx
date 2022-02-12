f337fe9c09ac0e710c345d7fb095f890a12d4e87
Alex <suhobski@mail.ru>
2022-02-12 23:32:13 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FooterContainer } from "./Footer.style";
+
+const Footer = () => (
+  <FooterContainer>
+    <p>February, 2022</p>
+  </FooterContainer>
+);
+
+export default Footer;
---
+++ src/components/Footer/Footer.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+  border-top: 2px solid #65655a;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
