14e76ac8349db87bebf823cb8b8bf8bd469dc9df
Alex <suhobski@mail.ru>
2022-02-19 15:48:24 +0300

    fix: change app height

--- src/pages/AppLayout/AppLayout.style.jsx
+++ src/pages/AppLayout/AppLayout.style.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const AppContainer = styled.div`
   display: grid;
   grid-template-rows: auto 1fr 30px;
+  height: 100%;
   max-width: 800px;
   margin: 0 auto;
   background-color: #eee;
